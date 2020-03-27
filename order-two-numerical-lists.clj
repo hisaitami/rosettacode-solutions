57cbb0e28c8440f6226bbce814eddb88bb1da9ae
hisaitami <hisaitami@gmail.com>
2020-03-28 03:43:17 +0900

    Simplify using compare function

--- order-two-numerical-lists.clj
+++ order-two-numerical-lists.clj
@@ -1,12 +1,8 @@
 ;; Task
 ;; @see https://rosettacode.org/wiki/Order_two_numerical_lists
 
-(defn lexicographic-order?
-  "Return true if the first list should be ordered before the second, otherwise false.
-   The order is determined by lexicographic order."
-  [coll1 coll2]
-  (if (> (count coll1) (count coll2)) false ; return false if the second list run out of elements
-      (every? true? (map <= coll1 coll2))))
+(defn lexicographic-order? [x y]
+  (> 1 (compare x y)))
 
 (lexicographic-order? [1 2 3] [1 2 4])
 ;;=> true
