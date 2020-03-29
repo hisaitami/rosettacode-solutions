ad8d6e16158a8f47de8c07f512bbeeb932dcd1fb
hisaitami <hisaitami@gmail.com>
2020-03-29 17:23:14 +0900

    Simplify the value checking process

--- babbage-problem.clj
+++ babbage-problem.clj
@@ -3,10 +3,7 @@
 
 (->> (range 1 99736)
      (map (fn [x]
-            (let [xx (* x x)
-                  a (* (Math/floor (/ xx 1000000)) 1000000)
-                  b (- xx 269696)]
-              (if (= a b) x nil))))
+            (when (= 269696 (mod (* x x) 1000000)) x)))
      (drop-while nil?)
      (first))
 ;;=> 25264
