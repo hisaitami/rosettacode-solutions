2d5ebd37fcd9b2d8f7176c8c60a27fb2cd3af7c1
hisaitami <hisaitami@gmail.com>
2020-04-12 14:26:02 +0900

    Apply ns

--- src/rosettacode_solutions/root-mean-square.clj
+++ src/rosettacode_solutions/root-mean-square.clj
@@ -1,9 +1,13 @@
 ;; Task
 ;; @see https://rosettacode.org/wiki/Averages/Root_mean_square
 
+(ns rosettacode-solutions.root-mean-square
+  (:require [rosettacode-solutions.math :as math]))
+
 (defn rms [xs]
   (let [n (count xs)
-        sigma (apply + (map #(* % %) xs))]
+        sigma (reduce + (map math/square xs))]
     (Math/sqrt (/ sigma n))))
 
-(rms (range 1 11))
+;;(rms [1 1 2 3 5])
+;;=> 2.8284271247461903
