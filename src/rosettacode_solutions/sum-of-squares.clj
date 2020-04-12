c8b1eb7d8f04b4b85be4988c129a6e356a02b6cc
hisaitami <hisaitami@gmail.com>
2020-04-12 12:23:54 +0900

    Apply ns

--- src/rosettacode_solutions/sum-of-squares.clj
+++ src/rosettacode_solutions/sum-of-squares.clj
@@ -1,12 +1,15 @@
 ;; Task
 ;; @see https://rosettacode.org/wiki/Sum_of_squares
 
+(ns rosettacode-solutions.sum-of-squares
+  (:require [rosettacode-solutions.math :as math]))
+
 (defn sum-of-squares [v]
-  (apply + (map #(* % %) v)))
+  (reduce + (map math/square v)))
 
-(sum-of-squares [1 2 3 4 5])
-;=> 55
+;;(sum-of-squares [1 2 3 4 5])
+;;=> 55
 
-; The program should work on a zero-length vector with an answer of 0.
-(sum-of-squares [])
-;=> 0
+;; The program should work on a zero-length vector with an answer of 0.
+;;(sum-of-squares [])
+;;=> 0
