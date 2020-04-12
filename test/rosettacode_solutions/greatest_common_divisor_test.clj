9bf7a9bff0d793b2f41f62bc896b8ff7c88127d3
hisaitami <hisaitami@gmail.com>
2020-04-13 08:00:27 +0900

    Refer gcd only

--- test/rosettacode_solutions/greatest_common_divisor_test.clj
+++ test/rosettacode_solutions/greatest_common_divisor_test.clj
@@ -1,9 +1,8 @@
 (ns rosettacode-solutions.greatest-common-divisor-test
   (:require [clojure.test :refer :all]
-            [rosettacode-solutions.greatest-common-divisor :as math]))
-
+            [rosettacode-solutions.greatest-common-divisor :refer [gcd]]))
 
 (deftest gcd-test
-  (is (= 1 (math/gcd 7 11)))
-  (is (= 4 (math/gcd 4 12)))
-  (is (= 14 (math/gcd 42 56))))
+  (is (= 1 (gcd 7 11)))
+  (is (= 4 (gcd 4 12)))
+  (is (= 14 (gcd 42 56))))
