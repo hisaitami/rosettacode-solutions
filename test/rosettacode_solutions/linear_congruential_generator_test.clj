63937b99f53fa78e9643c58a91c580d2525aee90
hisaitami <hisaitami@gmail.com>
2020-04-14 14:15:17 +0900

    Change a test for normal case

--- test/rosettacode_solutions/linear_congruential_generator_test.clj
+++ test/rosettacode_solutions/linear_congruential_generator_test.clj
@@ -3,7 +3,7 @@
   (:use [rosettacode-solutions.linear-congruential-generator]))
 
 (deftest lcg-test
-  (is (= 5 ((lcg 3 5 13) 0)))
+  (is (= [8 3 1 8 3] (take 5 (iterate (lcg 3 5 13) 8))))
   (testing "it should throw AssertionError if an invalid constant is given"
     (is (thrown-with-msg?
          AssertionError #"Assert failed: \(> m a\)"
