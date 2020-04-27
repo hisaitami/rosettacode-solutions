625b446f97da347bcce2880c533c5a8bd57a6f24
hisaitami <hisaitami@gmail.com>
2020-04-27 14:54:02 +0900

    Add a solution for the task Palindrome detection

---
+++ src/rosettacode_solutions/palindrome_detection.clj
@@ -0,0 +1,25 @@
+;; Task
+;; @see https://rosettacode.org/wiki/Palindrome_detection
+
+(ns rosettacode-solutions.palindrome-detection)
+
+(defn palindrome? [s]
+  (= (->> s str seq) (->> s str reverse)))
+
+;(palindrome? 123456789987654321)
+;;=> true
+
+;(palindrome? "SATOR AREPO TENET OPERA ROTAS")
+;;=> true
+
+;; Unicode characters
+;(palindrome? "NIψONANOMHMATAMHMONANOψIN")
+
+;; Detect palindrome if white-space and punctuation is ignored and case-insensitive
+;; (->> "No lemon, no melon"
+;;      (#(clojure.string/replace % #"[\s.,:;!?]+" ""))   ; ignore white-space and punctuation
+;;      (clojure.string/lower-case)                       ; case-insensitive comparison
+;;     palindrome?)
+;;=> true
+
+
